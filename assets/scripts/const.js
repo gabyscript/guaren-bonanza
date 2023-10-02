88909bb29078ee4cda5fdd372d28f5ffef92a603
Gabriel Preller <gpreller@geovictoria.com>
2023-10-02 20:44:15 -0300

    Inicio de proyecto

---
+++ assets/scripts/const.js
@@ -0,0 +1,26 @@
+let cardsConst = [
+    {nombre: "Sinpremio",
+    src: "https://i.postimg.cc/0y2CBjhL/Seleccionar-carta-2.png",
+    value: "NoPrice",
+    modalTitleText: "Mejor suerte la próxima vez 🥲",
+    modalDescriptionText: "Buena call, buena call 🥲"
+    },
+    {nombre: "Minor",
+    src: "https://i.postimg.cc/bYK6v1s8/minor-sin-borde.png",
+    value: "Minor",
+    modalTitleText: "🟢 Minor 🟢",
+    modalDescriptionText: "Premio chiquitito pero empeñoso 😳"
+    },
+    {nombre: "Major",
+    src: "https://i.postimg.cc/cHznX7VJ/major-sin-borde.png",
+    value: "Major",
+    modalTitleText: "🔵 Major 🔵",
+    modalDescriptionText: "Alta win papu 🤑"
+    },
+    {nombre: "Grand",
+     src: "https://i.postimg.cc/yYZFp56C/GRAND-sin-borde.png",
+     value: "Grand",
+     modalTitleText: "🔴 Grand 🔴",
+     modalDescriptionText: "Has sacado MAX WIN, pedacito 🤑"   
+    }
+]
---
+++ assets/scripts/main.js
@@ -0,0 +1,142 @@
+// Const Pagina
+
+const mainSection = document.getElementById('main-section')
+let reiniciarBtn = document.getElementById('resetButton')
+
+// Const Values
+
+const cardsValues = cardsConst.map(card => {return card.value})
+
+function calculatePrize(number) {
+    if (number >= 0.85) {
+        return cardsValues.find(value => value == "Grand")
+    } 
+    else if (number < 0.85 && number >= 0.55) {
+        return cardsValues.find(value => value == "Major")
+    } 
+    else {
+        return cardsValues.find(value => value == "Minor")
+    } 
+}
+
+function orderRandomly(array) {   
+
+    for (i = 0 ; i < 3 ; i++ ) {
+
+        let currentIndex = array.length, randomIndex;
+
+        while (currentIndex != 0 ) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+        }    
+    }
+    
+    return array;
+}
+
+function generateCards() {
+
+    const cards = [];
+    const randomSafeNumber = Math.random()
+
+    let totalNoPrize = 0;
+    let safePrize = 0;
+
+    while (totalNoPrize < 3 ) {            
+        let noPrizeCard = cardsConst.find(card => card.value == "NoPrice")
+        cards.push(noPrizeCard)
+        totalNoPrize++
+    }
+
+    while (safePrize < 3) {        
+        let safePrizeValue = calculatePrize(randomSafeNumber)
+        let safePrizeCard = cardsConst.find(card => card.value == safePrizeValue)
+        cards.push(safePrizeCard)
+        safePrize++
+    }
+
+    while (cards.length < 12) {
+        const randomValue = cardsValues[Math.floor(Math.random() * cardsValues.length)];
+        const randomCard = cardsConst.find(card => card.value == randomValue)
+        cards.push(randomCard)
+    }    
+    return cards
+}
+
+function showCards() {
+    
+    let gameCards = generateCards()
+    gameCards = orderRandomly(gameCards)
+
+    console.log(gameCards)
+
+    for(let card of gameCards) {
+
+        let reverseCard = document.createElement('div');
+        reverseCard.setAttribute('id', 'backcards')
+        reverseCard.classList.add('col-2')
+        reverseCard.classList.add('m-2')
+        reverseCard.dataset.value = card.value;   
+        reverseCard.dataset.text = card.modalTitleText;
+        reverseCard.dataset.description = card.modalDescriptionText;  
+
+        let frontCard = document.createElement('div');
+        frontCard.setAttribute('id', 'frontcards')
+        frontCard.classList.add('col')
+        frontCard.style.backgroundImage = `url(${card.src})`
+
+        let betSalaDiv = document.createElement('div');
+        betSalaDiv.classList.add('bet-sala-logo') 
+
+        mainSection.appendChild(reverseCard);
+        reverseCard.appendChild(frontCard)
+        reverseCard.appendChild(betSalaDiv)
+    }    
+}
+
+showCards()
+
+const cards = document.querySelectorAll("#backcards");
+cards.forEach(card => {
+    card.addEventListener('click', cardClickHandler)
+})
+
+let turnedCards = []
+
+function cardClickHandler(){
+    
+    const currentCard = this;
+    currentCard.classList.add('descubierta')
+
+    turnedCards.push(currentCard);
+
+    let checkedCards = turnedCards.filter(card => card.dataset.value == this.dataset.value)
+
+    if (checkedCards.length === 3 ) {
+
+        cards.forEach(card => {
+            card.classList.add('descubierta')
+        })        
+
+        let modalTitle = document.getElementById('modalTitle');
+        let modalDescription = document.getElementById('modalDescription');
+
+        console.log(currentCard.value)
+
+        if (currentCard.dataset.value === "NoPrice") {
+            modalTitle.innerHTML = `Siga participando <br> ${currentCard.dataset.text}`
+            modalDescription.innerHTML = `${currentCard.dataset.description}`
+        } else {
+            modalTitle.innerHTML = `Felicidades, has ganado el premio: <br> ${currentCard.dataset.text}`
+            modalDescription.innerHTML = `${currentCard.dataset.description}`
+        }        
+        
+        const modal = new bootstrap.Modal(document.getElementById('modal'));
+        modal.show();
+    }
+}
+
+reiniciarBtn.addEventListener('click', function(){
+    location.reload()
+})
